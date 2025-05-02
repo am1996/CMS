465f4420d9025935726025aa0d110953892163f2
Ahmed Magdy <ahmedmagdy92@yahoo.com>
2025-05-02 06:48:25 +0000

    configured for replit

--- CMS/settings.py
+++ CMS/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-(wkl00$nnfl=jdpqz9076!4(3rau61giext7uwhzb!1un58l#a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
